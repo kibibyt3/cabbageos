91126256ce2f0aadf6ec2733de1667b727a7b700
madkingcabbage <37877820+madkingcabbage@users.noreply.github.com>
2018-10-25 08:55:26 -0600

    Added colors.py

---
+++ mod/colors.py
@@ -0,0 +1,23 @@
+DEFAULT="\\e[39mDefault"
+BLACK="\\e[30mBlack"
+RED="\\e[31m"
+GREEN="\\e[32m"
+YELLOW="\\e[33m"
+BLUE="\\e[34m"
+MAGENTA="\\e[35m"
+CYAN="\\e[36m"
+
+# Because spelling is weird.
+LIGHT_GRAY="\\e[37m"
+LIGHT_GREY="\\e[37m"
+
+DARK_GRAY="\\e[90m"
+DARK_GREY="\\e[90m"
+
+LIGHT_RED="\\e[91m"
+LIGHT_GREEN="\\e[92m"
+LIGHT_YELLOW="\\e[93m"
+LIGHT_BLUE="\\e[94m"
+LIGHT_MAGENTA="\\e[95m"
+LIGHT_CYAN="\\e[96m"
+WHITE="\\e[97"
